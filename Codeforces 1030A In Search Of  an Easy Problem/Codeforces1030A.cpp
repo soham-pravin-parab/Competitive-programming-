159cd57c95671351e2548d5397acec0f8df6b7dc
soham-pravin-parab <sohamparabmail@gmail.com>
2026-05-08 11:41:18 -0400

    Add files via upload

---
+++ Codeforces 1030A In Search Of  an Easy Problem/Codeforces1030A.cpp
@@ -0,0 +1,23 @@
+
+#include <iostream>
+using namespace std;
+int main()
+{
+	int n;
+	cin>>n;
+	for(int i=0; i<n; i++) {
+		int a;
+		cin>>a;
+		if(a==1) {
+			cout<<"HARD\n";
+			return 0;
+		}
+	}
+
+	cout<<"EASY\n";
+
+
+
+
+	return 0;
+}
