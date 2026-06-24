2a2303afa03d44fc4c6dcfa0a8e9772a6f195066
soham-pravin-parab <sohamparabmail@gmail.com>
2026-06-24 08:45:40 -0400

    Add files via upload

---
+++ Codeforces 460A Vasya and socks/Codeforces460A.cpp
@@ -0,0 +1,19 @@
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n,m;
+    cin>>n>>m;
+    int days =0;
+    while(n>0){
+        days++;
+        n--;
+        if(days%m==0){
+            n++;
+        }
+    }
+    std::cout<<days;
+
+    return 0;
+}
