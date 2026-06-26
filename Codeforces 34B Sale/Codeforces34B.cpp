a0cea93929a1282de7cfcfaec1b93cfa56124ca1
soham-pravin-parab <sohamparabmail@gmail.com>
2026-06-26 11:54:22 -0400

    Add files via upload

---
+++ Codeforces 34B Sale/Codeforces34B.cpp
@@ -0,0 +1,27 @@
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+int main()
+{
+    int n,m;
+    cin>>n>>m;
+    vector<int>a(n);
+    for(int i=0;i<n;i++){
+        cin>>a[i];
+    }
+    sort(a.begin(),a.end());
+    int c=0;
+    for(int i=0;i<m;i++){
+        if(a[i]<0){
+            c+=a[i];
+        }
+        else{
+            break;
+        }
+    }
+    std::cout<<-c;
+
+    return 0;
+}
