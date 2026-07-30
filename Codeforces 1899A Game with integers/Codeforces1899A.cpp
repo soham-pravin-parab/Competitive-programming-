e80f79f3cb1b6a4a03755bedcab1de95d2c8eb5c
soham-pravin-parab <sohamparabmail@gmail.com>
2026-07-30 22:56:51 +0530

    Add files via upload

---
+++ Codeforces 1899A Game with integers/Codeforces1899A.cpp
@@ -0,0 +1,20 @@
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--){
+    int n;
+    cin>>n;
+    
+    
+    if(n%3==0){
+        cout<<"Second\n";
+    }
+    else{
+    std::cout<<"First \n";}
+}
+    return 0;
+}
