740b2bb11a0721de5b2fb4602727d24e78ad5006
soham-pravin-parab <sohamparabmail@gmail.com>
2026-09-10 21:30:30 +0530

    Add files via upload

---
+++ Codeforces 2195A Sieve of Erato67henes/Codeforces2195A.cpp
@@ -0,0 +1,27 @@
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--){
+    int n;
+    cin>>n;
+    bool found = false;
+    for(int i=0;i<n;++i){
+        int x;
+        cin>>x;
+        if(x==67){
+            found = true;
+        }
+    }
+    if(found){
+        cout<<"YES\n";
+    }
+    else{
+        cout<<"NO\n";
+    }
+    }
+    return 0;
+}
