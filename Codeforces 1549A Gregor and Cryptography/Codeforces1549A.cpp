43e64a9452b553c96547bcb74216987e30f5f329
soham-pravin-parab <sohamparabmail@gmail.com>
2026-09-23 05:58:13 +0530

    Add files via upload

---
+++ Codeforces 1549A Gregor and Cryptography/Codeforces1549A.cpp
@@ -0,0 +1,18 @@
+
+#include <iostream>
+using namespace std;
+void solve (){
+    int p;
+    cin>>p;
+    cout<<2<<" "<<p-1<<"\n";
+}
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--){
+        solve();
+    }
+    
+    return 0;
+}
